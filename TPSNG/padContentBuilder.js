90ab1df2df7aa418cdb4eb1ad2b148b89806db3d
Murtuza Boxwala <1mboxwala2@gmail.com>
2016-11-04 16:10:43 -0400

    emails and etherpads for 2017

--- TPSNG/padContentBuilder.js
+++ TPSNG/padContentBuilder.js
@@ -2,20 +2,15 @@
 var rowUtils = require("./rowUtils");
 
 module.exports = {
-  
-
   build: function (row) {
-    var typeToBuilderMap = {"Track":buildTrackContent, "Practice Space": buildPracticeSpaceContent, "Network Gathering": buildNetworkGatheringContent};
-    var builder = typeToBuilderMap[rowUtils.getType(row)];
-    var content = "<!DOCTYPE html><html><head><title></title></head><body>" + builder(row) + "</body>";
-    return content;
+    return "<!DOCTYPE html><html><head><title></title></head><body>" + buildContent(row) + "</body>";
   }
 };
 
 
 function buildContentSection(header, content) {
 	var section = "";
-	section += "<strong>"+header+"</strong>";
+	section += "<strong>" + header + "</strong>";
 	section += '<br>';
 	section += content;
 	section += '<br><br>'
@@ -23,86 +18,57 @@ function buildContentSection(header, content) {
 }
 
 
-function buildCoordinatorSection(row, coordinatorNumber, firstNameColumn, lastNameColumn, emailColumn, phoneNumberColumn, bioColumn) {
+function buildCoordinatorSection(row, coordinatorNumber) {
 
-	if(!coordinatorEntered(row, firstNameColumn, lastNameColumn, emailColumn, phoneNumberColumn, bioColumn))
+	var nameColumn  =  "coordinator" + coordinatorNumber + "name";
+	var emailColumn =  "coordinator" + coordinatorNumber + "email";
+	var bioColumn   =  "coordinator" + coordinatorNumber + "bio";
+
+	var name  = row[nameColumn];
+	var email = row[emailColumn];
+	var bio   = row[bioColumn];
+
+	if(!coordinatorEntered(name, email, bio))
 		return "";
 
 	var sectionContent = "<strong>" + "Coordinator " + coordinatorNumber + "</strong>" + "<br>";
-	sectionContent += row[firstNameColumn] + " " + row[lastNameColumn] + ", ";
-	sectionContent += row[emailColumn] + ", ";
-	sectionContent += row[phoneNumberColumn] + "<br>";
-	sectionContent += row[bioColumn] + "<br>";
-	sectionContent += "<br>";
+
+	sectionContent += name  + ", ";
+	sectionContent += email + "<br>";
+	sectionContent += bio   + "<br>";
+	sectionContent +=         "<br>";
 	return sectionContent;
 }
 
-function coordinatorEntered(row, firstNameColumn, lastNameColumn, emailColumn, phoneNumberColumn, bioColumn) {
-	return row[firstNameColumn] || row[lastNameColumn] || row[emailColumn] || row[phoneNumberColumn] || row[bioColumn];
+function coordinatorEntered(name, email, bio) {
+	return name || email || bio;
 }
 
-function buildTrackContent (row) {
+function buildContent (row) {
+
 	var padContent = "";
-	
-	padContent += buildContentSection("Track Name", row['whatisyourtracksname']);
-	padContent += buildContentSection("Involving Media-based Organizing", row['pleasedescribehowthistrackinvolvesmedia-basedorganizing.']);
-	padContent += buildContentSection("Skills you Hope to Foster", row['pleasedescribesomeconcreteskillsthatyouhopeyourtrackwillfosterinparticipants.']);
-	padContent += buildContentSection("Description", row['pleasewriteafivesentencedescriptionofyourtrack.']);
-	padContent += buildContentSection("Invitees", row['whoaresomepeopleorganizationsnetworksmedia-makersyouwouldliketoinvitetoparticipateinyourtrack']);
 
+	var type = row['areyouproposingatrackpracticespaceornetworkgathering'];
+	padContent += buildContentSection("Name of your " + type, row['nameofyourtpsng']);
+	padContent += buildContentSection("Involving Media-based Organizing", row['howisyourcontentconnectedtomedia-basedorganizingmax.250words']);
+	padContent += buildContentSection("Description", row['pleasewritea5-sentencedescriptionofyourtpsngmax250words']);
+	padContent += buildContentSection("Additional Info", row['pleasewritea5-sentencedescriptionofyourtpsngmax250words']);		
 
-	padContent += buildCoordinatorSection(row, 1, 'coordinator1firstname', 'coordinator1lastname', 'coordinator1emailaddress',  'coordinator1phonenumber', 'coordinator1organizationalaffiliationsbio');
-	padContent += buildCoordinatorSection(row, 2, 'coordinator2firstname', 'coordinator2lastname', 'coordinator2emailaddress',  'coordinator2phonenumber', 'coordinator2organizationalaffiliationsbio');
-	padContent += buildCoordinatorSection(row, 3, 'coordinator3firstname', 'coordinator3lastname', 'coordinator3emailaddress',  'coordinator3phonenumber', 'coordinator3organizationalaffiliationsbio');
-	padContent += buildCoordinatorSection(row, 4, 'coordinator4firstname', 'coordinator4lastname', 'coordinator4emailaddress',  'coordinator4phonenumber', 'coordinator4organizationalaffiliationsbio');
-	padContent += buildCoordinatorSection(row, 5, 'coordinator5firstname', 'coordinator5lastname', 'coordinator5emailaddress',  'coordinator5phonenumber', 'coordinator5organizationalaffiliationsbio');
-	padContent += buildCoordinatorSection(row, 6, 'coordinator6firstname', 'coordinator6lastname', 'coordinator6emailaddress',  'coordinator6phonenumber', 'coordinator6organizationalaffiliationsbio');
+	padContent += buildCoordinators(row);
 		
 	return padContent;
 }
 
-function buildPracticeSpaceContent(row) {
-	var padContent = "";
-
-	padContent += buildContentSection("Practice Space Name", row['whatisyourpracticespacesname']);
-	padContent += buildContentSection("Vision", row['pleasedescribeyourvisionforwhatwillhappeninyourpracticespaceoverthethreedaysoftheamcfriday-sunday.']);
-	padContent += buildContentSection("Shareable Output", row['whatwillbetheshareablemediaarttechnologyorperformanceprojectcreatedinyourpracticespace']);
-	padContent += buildContentSection("Major Questions to Explore", row['whatmajorquestionswillyourpracticespaceexplorethroughtheweekend']);
-	padContent += buildContentSection("Invitees", row['whoaresomepeopleorganizationsnetworksmedia-makersyouwouldliketoinvitetoparticipateinyourpracticespace']);
-
-	padContent += buildCoordinatorSection(row, 1, 'coordinator1firstname_2', 'coordinator1lastname_2', 'coordinator1emailaddress_2',  'coordinator1phonenumber_2', 'coordinator1organizationalaffiliationsbio_2');
-	padContent += buildCoordinatorSection(row, 2, 'coordinator2firstname_2', 'coordinator2lastname_2', 'coordinator2emailaddress_2',  'coordinator2phonenumber_2', 'coordinator2organizationalaffiliationsbio_2');
-	padContent += buildCoordinatorSection(row, 3, 'coordinator3firstname_2', 'coordinator3lastname_2', 'coordinator3emailaddress_2',  'coordinator3phonenumber_2', 'coordinator3organizationalaffiliationsbio_2');
-	padContent += buildCoordinatorSection(row, 4, 'coordinator4firstname_2', 'coordinator4lastname_2', 'coordinator4emailaddress_2',  'coordinator4phonenumber_2', 'coordinator4organizationalaffiliationbio');
-	padContent += buildCoordinatorSection(row, 5, 'coordinator5firstname_2', 'coordinator5lastname_2', 'coordinator5emailaddress_2',  'coordinator5phonenumber_2', 'coordinator5organizationalaffiliationsbio_2');
-	padContent += buildCoordinatorSection(row, 6, 'coordinator6firstname_2', 'coordinator6lastname_2', 'coordinator6emailaddress_2',  'coordinator6phonenumber_2', 'coordinator6organizationalaffiliationsbio_2');
-	
-	
-	return padContent;
+function buildCoordinators(row) {
+	var content = "";
+	content += buildCoordinatorSection(row, 1);
+	content += buildCoordinatorSection(row, 2);
+	content += buildCoordinatorSection(row, 3);
+	content += buildCoordinatorSection(row, 4);
+	content += buildCoordinatorSection(row, 5);
+	content += buildCoordinatorSection(row, 6);
+	return content;
 }
-
-function buildNetworkGatheringContent(row) {
-
-	var padContent = "";
-
-	padContent += buildContentSection("Network Gathering Name", row['whatisthenameofyournetworkgathering']);
-	padContent += buildContentSection("Vision", row['describeyourvisionofwhatwilltakeplaceinyournetworkgathering.']);
-	padContent += buildContentSection("Relation to Media-based Organizing", row['howwillyournetworkgatheringrelatetomedia-basedorganizing']);
-	padContent += buildContentSection("Open or Invite Only?", row['willthisgatheringbeopentoallamcregistrantsorinvite-only']);
-	padContent += buildContentSection("Anticipated Number of Participants", row['whatistheanticipatednumberofparticipantsinyourng']);
-	padContent += buildContentSection("Invitees", row['whoaresomepeopleorganizationsnetworksmedia-makersyouwouldliketoinvitetoparticipateinyournetworkgathering']);
-    padContent += buildContentSection("Will you Encourage your Participants to Present Sessions and Participate in Other Content Areas", row['willyouencourageparticipantsinyournetworkgatheringtopresentsessionsparticipateinothercontentareasthroughtheweekend']);
-	
-
-	padContent += buildCoordinatorSection(row, 1, 'coordinator1firstname_3', 'coordinator1lastname_3', 'coordinator1emailaddress_3',  'coordinator1phonenumber_3', 'coordinator1organizationalaffiliationbio');
-	padContent += buildCoordinatorSection(row, 2, 'coordinator2firstname_3', 'coordinator2lastname_3', 'coordinator2emailaddress_3',  'coordinator2phonenumber_3', 'coordinator2organizationalaffiliationsbio_3');
-	padContent += buildCoordinatorSection(row, 3, 'coordinator3firstname_3', 'coordinator3lastname_3', 'coordinator3emailaddress_3',  'coordinator3phonenumber_3', 'coordinator3organizationalaffiliationbio');
-	padContent += buildCoordinatorSection(row, 4, 'coordinator4firstname_3', 'coordinator4lastname_3', 'coordinator4emailaddress_3',  'coordinator4phonenumber_3', 'coordinator4organizationalaffiliationbio_2');
-	padContent += buildCoordinatorSection(row, 5, 'coordinator5firstname_3', 'coordinator5lastname_3', 'coordinator5emailaddress_3',  'coordinator5phonenumber_3', 'coordinator5organizationalaffiliationbio');
-	padContent += buildCoordinatorSection(row, 6, 'coordinator6firstname_3', 'coordinator6lastname_3', 'coordinator6emailaddress_3',  'coordinator6phonenumber_3', 'coordinator6organizationalaffiliationsbio_3');
-	return padContent;
-	}
-
 
 
 
--- TPSNG/sessionProposals.js
+++ TPSNG/sessionProposals.js
@@ -3,8 +3,6 @@ var Tabletop = require('tabletop'),
 
 var padContentBuilder = require("./padContentBuilder");
 
-var rowUtils = require("./rowUtils");
-
 
 var ep = etherpadLiteClient.connect({
   apikey: '9e7d36e7a4a510484d523f29753a8c0079bdb008ec64e7749911c76e4118a185',
@@ -17,14 +15,15 @@ var ETHERPADS_ID_CHAR_SIZE_LIMIT = 50;
 function processSessionsSheet (data, tabletop) {
 
   
-  var sheet = data['AMC2016 TPSNG Proposal responses'];
+  var sheet = data['Form Responses 1'];
   var rows = sheet.elements;
   var numberOfRows = rows.length;
-  for(var i = 0; i < numberOfRows; i++) {
+  for(var i = 0; i < 1; i++) {
+    
     var row = rows[i];
     var padId = getPadId(row);
     var padContent = padContentBuilder.build(row);
-    console.log("https://etherpad.alliedmedia.org/p/" + padId);
+    console.log("http://etherpad.alliedmedia.org/p/" + padId);
     var callback = function (padIdClosure, padContentClosure) {
             return function(error, data)
             {
@@ -32,6 +31,7 @@ function processSessionsSheet (data, tabletop) {
             };
         }(padId, padContent);
     ep.createPad({padID: padId}, callback);
+    
   }
 }
 
@@ -43,31 +43,23 @@ function getPadId(row) {
 
 
 function getTitle(row) {
-  var typeToRowMap = 
-  {
-    'Track': 'whatisyourtracksname', 
-    'Practice Space': 'whatisyourpracticespacesname',
-    'Network Gathering': 'whatisthenameofyournetworkgathering'
-  };
-  var type = rowUtils.getType(row);
-  var titleRow = typeToRowMap[type];
-  var title = row[titleRow];
-
-  return title;
+  return row['nameofyourtpsng'];
 }
 
 function convertTitleToUrl(title) {
-  return ("2016_TPSNG_" + title.replace(/\W+/g, '_').toLowerCase()).substring(0,ETHERPADS_ID_CHAR_SIZE_LIMIT);
+  return ("2017_Test_TPSNG_" + title.replace(/\W+/g, '_').toLowerCase()).substring(0,ETHERPADS_ID_CHAR_SIZE_LIMIT);
 }
 
 
 function createPadCallback(error, data, padId, padContent) {
   if(error)
   {
+    console.error("error form creating etherpad")
     console.error(error);
     return;
   }
   if(data) {
+    console.log("data from creating etherpad")
     console.log(data);
     return;
   }
@@ -92,7 +84,7 @@ function setTextCallback(error, data) {
 function buildEtherPadUrl() {}
 
 Tabletop.init({
-  key: "1AX6La6Pn8jxS00Jr1QKuoo57VlPF2u4ILyN1PvXfd30",
+  key: "1dtciN2yY2KOHiT4YGPRaSM_5evhus79E9__vXwCQB2o",
   callback: processSessionsSheet,
   prettyColumnNames: false,
   simpleSheet: false
